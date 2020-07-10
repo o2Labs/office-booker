1aa8da7a6967d9722b25e680f68cb31b19ce0f6a
aangelisc <andreas.christou@telefonica.com>
2020-07-10 14:57:23 +0100

    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    Update tests
    
    # The commit message #2 will be skipped:
    
    # Update test script to encompass building
    #
    # - Use updated test script in build script

--- test.sh
+++ test.sh
@@ -3,12 +3,18 @@ set -e # stop on error
 
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
-unset AWS_SECURITY_TOKEN
 export AWS_ACCESS_KEY_ID=fakeKeyID
 export AWS_SECRET_ACCESS_KEY=fakeAccessKey
-run_tests() {
-    cd server
-    yarn test
-}
-run_tests
+
+docker-compose up -d
+
+cd server
+npx tsc --noEmit
+yarn test
+
+cd ../client
+npx tsc --noEmit
+yarn test
+
+cd ../alerts
+npx tsc --noEmit
