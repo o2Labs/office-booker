46fddaf66816d348880b3f0373797cf6d3ad8432
Richard Chapman <rcchapman85@gmail.com>
2020-07-21 14:29:59 +0100

    Availability icons and style structure

--- client/src/components/App/Home/MakeBooking.styles.ts
+++ client/src/components/App/Home/MakeBooking.styles.ts
@@ -187,57 +187,45 @@ export default styled.div`
         > .right {
           flex: 1 1 70%;
 
-          display: flex;
-          justify-content: flex-end;
+          text-align: right;
 
-          .availability {
+          > .no-booking {
             display: flex;
             justify-content: flex-end;
-            width: 100%;
-            margin-right: 1rem;
 
-            svg {
-              margin-right: 0.3rem;
-              font-size: 2rem;
-            }
-            button {
-              padding: 0;
-              margin: 0.5rem 0 0 0.5rem;
+            > .availability {
+              display: flex;
+              align-items: center;
+              justify-content: flex-end;
+
+              margin-right: 1rem;
+
+              > .status {
+                flex: 0 0 auto;
+
+                display: flex;
+
+                > svg {
+                  flex: 0 0 auto;
+                  font-size: 2rem;
+                }
+
+                > p {
+                  flex: 1 1 auto;
+
+                  margin: 0 0 0 0.4rem;
+                }
+
+                &:not(:first-of-type) {
+                  padding-left: 1rem;
+                }
+              }
             }
           }
 
           .cancelBtn {
-            margin-right: 1rem;
+            margin-right: 1.2rem;
           }
-
-          /* > p {
-            margin: 0;
-
-            &.booked {
-              color: ${(props) => props.theme.palette.primary.main};
-              font-size: 1.8rem;
-              line-height: 1.8rem;
-              font-weight: bold;
-              text-transform: uppercase;
-            }
-
-            &.available {
-              color: ${(props) => props.theme.palette.secondary.main};
-              font-weight: bold;
-            }
-          } */
-
-          /* > button {
-            margin-top: 2rem;
-
-            color: ${(props) => props.theme.palette.secondary.main};
-            font-size: 1.2rem;
-            font-weight: bold;
-
-            &.loading {
-              color: ${(props) => props.theme.palette.text.disabled};
-            }
-          } */
         }
       }
     }
