1389382be2c5cc9f12e3443ca41312651424cabf
Richard Chapman <rcchapman85@gmail.com>
2020-07-23 17:09:30 +0100

    Users restructure

--- client/src/components/App/Admin/Users.styles.ts
+++ client/src/components/App/Admin/Users.styles.ts
@@ -2,157 +2,74 @@ import styled from 'styled-components';
 
 export default styled.div`
   ${(props) => props.theme.breakpoints.up('xs')} {
-    margin: 0 2rem 2.4rem;
-    .MuiPaper-root {
-      padding: 2rem 2rem 2.4rem;
-    }
+    padding: 0 2rem 2rem;
   }
 
   ${(props) => props.theme.breakpoints.up('sm')} {
-    margin: 0 3rem 4rem;
-    .MuiPaper-root {
-      padding: 3rem 3rem 4rem;
-    }
+    padding: 0 3rem 3rem;
   }
 
-  margin: 0 3rem;
+  > h3 {
+    margin: 0 0 2rem;
 
-  p,
-  label {
-    color: ${(props) => props.theme.palette.secondary.main};
-    font-size: 1.6rem;
-    font-weight: 400;
-
-    margin: 0;
-
-    > a {
-      color: ${(props) => props.theme.palette.primary.main};
-    }
+    color: ${(props) => props.theme.palette.primary.main};
+    font-size: 1.8rem;
+    font-weight: 500;
   }
 
-  .breadcrumb-text {
-    margin: 0;
-  }
-
-  .breadcrumb-text.previous {
-    color: #000000de;
-  }
-
-  .filters {
-    margin-bottom: 2rem;
-    display: flex;
-
-    ${(props) => props.theme.breakpoints.up('xs')} {
-      flex-direction: column;
-
-      .search-user {
-        .MuiFormControl-root {
-          width: 100%;
-        }
-      }
-
-      .filter-roles {
-        margin-top: 3rem;
-      }
-    }
-
-    ${(props) => props.theme.breakpoints.up('sm')} {
-      flex-direction: row;
-      justify-content: space-between;
-
-      .search-user {
-        margin-right: 4rem;
-
-        .MuiFormControl-root {
-          min-width: 30rem;
-        }
-      }
-
-      .filter-roles {
-        margin: 0rem;
-      }
-    }
-
-    .search-user {
+  > .table-container {
+    > .filters {
       display: flex;
-      align-items: center;
-      form {
-        width: 100%;
-      }
-    }
 
-    .filter-roles {
-      .MuiSelect-select {
-        min-width: 12rem;
-      }
-    }
-  }
-
-  .edit-user {
-    form {
-      .user-email-tt {
-        font-size: 2.5rem;
-        margin: 2rem 0;
-      }
-
-      h4 {
-        margin-bottom: 2rem;
-        color: #000000de;
-      }
-
-      .MuiFormControl-root {
-        min-width: 25rem;
-
-        margin-right: 1rem;
-      }
-
-      input {
-        font-size: 2.5rem;
-      }
-
-      .buttons {
-        margin-top: 2rem;
-
-        button {
-          margin-right: 1rem;
-        }
-      }
-
-      .role-container {
-        display: flex;
+      ${(props) => props.theme.breakpoints.only('xs')} {
         flex-direction: column;
+      }
 
-        margin-bottom: 2rem;
+      ${(props) => props.theme.breakpoints.up('sm')} {
+        align-items: center;
+        justify-content: space-between;
+      }
 
-        .MuiFormControl-root {
-          margin-bottom: 1.5rem;
-        }
+      padding: 3rem 3rem 2rem;
 
-        .chips-select {
-          .MuiSelect-select {
-            overflow: scroll;
-          }
+      > .filter-roles {
+        flex: 0 0 auto;
+      }
+
+      > .search-user {
+        flex: 0 0 auto;
+
+        ${(props) => props.theme.breakpoints.only('xs')} {
+          padding-top: 2rem;
         }
       }
     }
-  }
 
-  .docs {
-    margin-top: 1em;
-
-    > p,
-    ul > li {
-      color: ${(props) => props.theme.palette.secondary.main};
+    > .table {
+      padding: 0 1.6rem 3rem;
+      overflow: scroll;
     }
   }
 
-  .listing-container {
-    overflow: scroll;
+  > .load-more-container {
+    padding: 0 3rem 3rem;
+
+    text-align: center;
   }
 
-  .load-more-container {
-    display: flex;
-    justify-content: center;
-    margin: 1rem 0;
+  > .unregistered-user {
+    padding: 0 3rem 3rem;
+
+    > p {
+      color: ${(props) => props.theme.palette.secondary.main};
+      font-size: 1.6rem;
+      font-weight: 400;
+
+      margin: 0;
+
+      > a {
+        color: ${(props) => props.theme.palette.primary.main};
+      }
+    }
   }
 `;
--- client/src/components/App/Admin/Users.tsx
+++ client/src/components/App/Admin/Users.tsx
@@ -1,22 +1,23 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { RouteComponentProps, Link } from '@reach/router';
-import {
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
-  InputLabel,
-  Select,
-  MenuItem,
-  FormControl,
-} from '@material-ui/core';
-import Create from '@material-ui/icons/Create';
-import { TextField, InputAdornment } from '@material-ui/core';
-import Search from '@material-ui/icons/Search';
+import TableContainer from '@material-ui/core/TableContainer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableBody from '@material-ui/core/TableBody';
+import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import CreateIcon from '@material-ui/icons/Create';
+import SearchIcon from '@material-ui/icons/Search';
 
 import { AppContext } from '../../AppProvider';
+import Loading from '../../Assets/LoadingSpinner';
 
 import AdminLayout from './Layout/Layout';
 import { OurButton } from '../../../styles/MaterialComponents';
@@ -26,10 +27,13 @@ import { User, UserQuery } from '../../../types/api';
 import { queryUsers } from '../../../lib/api';
 import { formatError } from '../../../lib/app';
 
-import UsersStyles from './User.styles';
+import UsersStyles from './Users.styles';
 
 // Types
-type UserFilter = { name: 'System Admin' | 'Office Admin' | 'custom' | 'all'; email?: string };
+type UserFilter = {
+  name: 'System Admin' | 'Office Admin' | 'custom' | 'all';
+  email?: string;
+};
 
 // Helpers
 const userFilterToQuery = (filter: UserFilter): UserQuery => {
@@ -50,26 +54,40 @@ const Users: React.FC<RouteComponentProps> = () => {
   // Global state
   const { state, dispatch } = useContext(AppContext);
   const { user } = state;
+
+  // Local state
+  const [loading, setLoading] = useState(true);
   const [queryResult, setQueryResult] = useState<User[] | undefined>(undefined);
   const [paginationToken, setPaginationToken] = useState<string | undefined>(undefined);
   const [selectedFilter, setSelectedFilter] = useState<UserFilter>({ name: 'System Admin' });
   const [email, setEmail] = useState<string>('');
 
+  // Effects
   useEffect(() => {
-    setQueryResult(undefined);
     queryUsers(userFilterToQuery(selectedFilter))
       .then((result) => {
         setQueryResult(result.users);
         setPaginationToken(result.paginationToken);
       })
-      .catch((error) =>
+      .catch((err) => {
+        // Handle errors
+        setLoading(false);
+
         dispatch({
           type: 'SET_ERROR',
-          payload: formatError(error),
-        })
-      );
+          payload: formatError(err),
+        });
+      });
   }, [selectedFilter, dispatch]);
 
+  useEffect(() => {
+    if (queryResult) {
+      // Wait for local state to be ready
+      setLoading(false);
+    }
+  }, [queryResult]);
+
+  // Handlers
   const handleSelectedRoleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     const val = event.target.value;
     if (val === 'System Admin' || val === 'Office Admin' || val === 'custom' || val === 'all') {
@@ -94,6 +112,7 @@ const Users: React.FC<RouteComponentProps> = () => {
     }
   };
 
+  // Render
   if (!user) {
     return null;
   }
@@ -101,99 +120,113 @@ const Users: React.FC<RouteComponentProps> = () => {
   return (
     <AdminLayout currentRoute="users">
       <UsersStyles>
-        <Paper square>
-          <h3>View Users</h3>
-          <section className="filters">
-            <div className="filter-roles">
-              <FormControl variant="outlined">
-                <InputLabel style={{ backgroundColor: '#ffffff' }}>Select Filter</InputLabel>
-                <Select value={selectedFilter.name} onChange={handleSelectedRoleChange}>
-                  <MenuItem value={'System Admin'}>System Admins</MenuItem>
-                  <MenuItem value={'Office Admin'}>Office Admins</MenuItem>
-                  <MenuItem value={'custom'}>Users with Custom Quota</MenuItem>
-                  <MenuItem value={'all'}>All Registered Users</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
+        {' '}
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <h3>Users</h3>
 
-            <div className="search-user">
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  const sanitisedEmail = email.trim().toLowerCase();
-                  setSelectedFilter((filter) => ({
-                    ...filter,
-                    email: sanitisedEmail === '' ? undefined : sanitisedEmail,
-                  }));
-                }}
-              >
-                <TextField
-                  placeholder="Start of email address"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <Search />
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </form>
-            </div>
-          </section>
+            <Paper square className="table-container">
+              <section className="filters">
+                <div className="filter-roles">
+                  <FormControl variant="outlined">
+                    <InputLabel style={{ backgroundColor: '#ffffff' }}>Select Filter</InputLabel>
+                    <Select value={selectedFilter.name} onChange={handleSelectedRoleChange}>
+                      <MenuItem value={'System Admin'}>System Admins</MenuItem>
+                      <MenuItem value={'Office Admin'}>Office Admins</MenuItem>
+                      <MenuItem value={'custom'}>Users with Custom Quota</MenuItem>
+                      <MenuItem value={'all'}>All Registered Users</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
 
-          <section className="listing-container">
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>User Email</TableCell>
-                  <TableCell>Quota</TableCell>
-                  <TableCell>Role</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {!queryResult
-                  ? null
-                  : queryResult.map((user) => (
-                      <TableRow key={user.email}>
-                        <TableCell>
-                          <Link to={`/admin/users/${user.email}`}>
-                            {user.email} <Create />
-                          </Link>
-                        </TableCell>
-                        <TableCell>{user.quota}</TableCell>
-                        <TableCell>{user.role.name}</TableCell>
-                      </TableRow>
-                    ))}
-              </TableBody>
-            </Table>
-          </section>
-
-          {paginationToken && (
-            <section className="load-more-container">
-              <OurButton onClick={loadMore} variant="contained">
-                Load More
-              </OurButton>
-            </section>
-          )}
-
-          {selectedFilter.name === 'all' &&
-            selectedFilter.email !== undefined &&
-            validateEmail(state.config?.emailRegex, selectedFilter.email) &&
-            queryResult?.length === 0 && (
-              <section className="unregistered-user">
-                <p>
-                  User not yet registered, edit{' '}
-                  <Link to={`/admin/users/${selectedFilter.email}`}>
-                    {selectedFilter.email}
-                    <Create />
-                  </Link>{' '}
-                  anyway.
-                </p>
+                <div className="search-user">
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      const sanitisedEmail = email.trim().toLowerCase();
+                      setSelectedFilter((filter) => ({
+                        ...filter,
+                        email: sanitisedEmail === '' ? undefined : sanitisedEmail,
+                      }));
+                    }}
+                  >
+                    <TextField
+                      placeholder="Start of email address"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <SearchIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </form>
+                </div>
               </section>
-            )}
-        </Paper>
+
+              <TableContainer className="table">
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>User Email</TableCell>
+                      <TableCell>Quota</TableCell>
+                      <TableCell>Role</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {queryResult && queryResult.length > 0 ? (
+                      queryResult.map((user) => (
+                        <TableRow key={user.email}>
+                          <TableCell>
+                            <Link to={`/admin/users/${user.email}`}>
+                              {user.email} <CreateIcon />
+                            </Link>
+                          </TableCell>
+                          <TableCell>{user.quota}</TableCell>
+                          <TableCell>{user.role.name}</TableCell>
+                        </TableRow>
+                      ))
+                    ) : (
+                      <TableRow>
+                        <TableCell>No users found</TableCell>
+                        <TableCell />
+                        <TableCell />
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+
+              {paginationToken && (
+                <section className="load-more-container">
+                  <OurButton onClick={loadMore} variant="contained">
+                    Load More
+                  </OurButton>
+                </section>
+              )}
+
+              {selectedFilter.name === 'all' &&
+                selectedFilter.email !== undefined &&
+                validateEmail(state.config?.emailRegex, selectedFilter.email) &&
+                queryResult?.length === 0 && (
+                  <section className="unregistered-user">
+                    <p>
+                      User not yet registered, edit{' '}
+                      <Link to={`/admin/users/${selectedFilter.email}`}>
+                        {selectedFilter.email}
+                        <CreateIcon />
+                      </Link>{' '}
+                      anyway.
+                    </p>
+                  </section>
+                )}
+            </Paper>
+          </>
+        )}
       </UsersStyles>
     </AdminLayout>
   );
