956d9ed0b97dc81074ffc5c21bc3158443462f28
Daniel Bradley <daniel.bradley@telefonica.com>
2020-09-08 15:04:56 +0100

    Move user defaulting logic out of DB layer
    - Keep DB interface the same as the DB entity.

--- server/db/users.ts
+++ server/db/users.ts
@@ -4,15 +4,15 @@ import { attribute, table, hashKey } from '@aws/dynamodb-data-mapper-annotations
 import { DataMapper } from '@aws/dynamodb-data-mapper';
 import { FunctionExpression, AttributePath } from '@aws/dynamodb-expressions';
 
-export interface User {
+export interface DbUser {
   email: string;
-  quota: number;
-  adminOffices: string[];
+  quota?: number;
+  adminOffices?: string[];
   created: string;
 }
 
 @table('users')
-export class UserModel {
+export class UserModel implements DbUser {
   @hashKey()
   email!: string;
   @attribute()
@@ -22,17 +22,8 @@ export class UserModel {
   @attribute({
     defaultProvider: () => new Date().toISOString(),
   })
-  created?: string;
+  created!: string;
 }
-
-const toUser = (config: Config, dbUser: UserModel) => {
-  return {
-    email: dbUser.email,
-    quota: dbUser.quota || config.defaultWeeklyQuota,
-    adminOffices: dbUser.adminOffices || [],
-    created: dbUser.created ?? new Date().toISOString(),
-  };
-};
 
 const buildMapper = (config: Config) =>
   new DataMapper({
@@ -40,16 +31,16 @@ const buildMapper = (config: Config) =>
     tableNamePrefix: config.dynamoDBTablePrefix,
   });
 
-export const getAllUsers = async (config: Config): Promise<User[]> => {
+export const getAllUsers = async (config: Config): Promise<DbUser[]> => {
   const mapper = buildMapper(config);
-  const rows: User[] = [];
+  const rows: UserModel[] = [];
   for await (const item of mapper.scan(UserModel)) {
-    rows.push(toUser(config, item));
+    rows.push(item);
   }
   return rows;
 };
 
-export const getUsersDb = async (config: Config, userEmails: string[]): Promise<User[]> => {
+export const getUsersDb = async (config: Config, userEmails: string[]): Promise<DbUser[]> => {
   const mapper = buildMapper(config);
   const emailsLowered = userEmails.map((e) => e.toLowerCase());
   const users = [];
@@ -60,20 +51,21 @@ export const getUsersDb = async (config: Config, userEmails: string[]): Promise<
     users.push(result);
   }
   const usersByEmail = new Map(users.map((u) => [u.email, u]));
-  return emailsLowered.map((email) => toUser(config, usersByEmail.get(email) ?? { email }));
+  return emailsLowered.map(
+    (email) => usersByEmail.get(email) ?? { email, created: new Date().toISOString() }
+  );
 };
 
-export const getUserDb = async (config: Config, userEmail: string): Promise<User> => {
+export const getUserDb = async (config: Config, userEmail: string): Promise<DbUser> => {
   const mapper = buildMapper(config);
   const email = userEmail.toLocaleLowerCase();
 
   try {
     const result = await mapper.get(Object.assign(new UserModel(), { email }));
-    console.log(result);
-    return toUser(config, result);
+    return result;
   } catch (err) {
     if (err.name === 'ItemNotFoundException') {
-      return toUser(config, { email });
+      return { email, created: new Date().toISOString() };
     } else {
       throw err;
     }
@@ -82,23 +74,15 @@ export const getUserDb = async (config: Config, userEmail: string): Promise<User
 
 export const ensureUserExists = async (
   config: Config,
-  user: User
+  user: DbUser
 ): Promise<{ userCreated: boolean }> => {
   const mapper = buildMapper(config);
 
   // Ensure object exists
   try {
-    await mapper.put(
-      Object.assign(new UserModel(), {
-        email: user.email,
-        quota: user.quota === config.defaultWeeklyQuota ? undefined : user.quota,
-        adminOffices: user.adminOffices.length === 0 ? undefined : user.adminOffices,
-        created: new Date().toISOString(),
-      }),
-      {
-        condition: new FunctionExpression('attribute_not_exists', new AttributePath('email')),
-      }
-    );
+    await mapper.put(Object.assign(new UserModel(), user), {
+      condition: new FunctionExpression('attribute_not_exists', new AttributePath('email')),
+    });
     return { userCreated: true };
   } catch (err) {
     if (err.code !== 'ConditionalCheckFailedException') {
@@ -108,7 +92,7 @@ export const ensureUserExists = async (
   }
 };
 
-export const setUser = async (config: Config, user: User): Promise<void> => {
+export const setUser = async (config: Config, user: DbUser): Promise<void> => {
   const mapper = buildMapper(config);
 
   const { userCreated } = await ensureUserExists(config, user);
@@ -118,7 +102,7 @@ export const setUser = async (config: Config, user: User): Promise<void> => {
       Object.assign(new UserModel(), {
         email: user.email,
         quota: user.quota === config.defaultWeeklyQuota ? undefined : user.quota,
-        adminOffices: user.adminOffices.length === 0 ? undefined : user.adminOffices,
+        adminOffices: (user.adminOffices?.length ?? 0) === 0 ? undefined : user.adminOffices,
         create: user.created,
       })
     );
