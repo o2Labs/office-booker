79704f297e50a94001a18ac6b614e5e355cd74cf
AtifKhokhar <atif.khokhar@telefonica.com>
2020-09-09 15:14:50 +0100

    implemented banner for IE users encouraging them to use modern browsers.

--- client/src/components/App/Home.styles.ts
+++ client/src/components/App/Home.styles.ts
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
 
-export default styled.div``;
+export default styled.div`
+  .ie-banner {
+    background: #fff4e5;
+    color: #663c00;
+    overflow: hidden;
+    padding: 0;
+    position: relative;
+    z-index: 2;
+    max-height: 0px;
+    transition: 1s;
+    transform: translate3d(0, 0, 0);
+  }
+
+  .ie-banner--open {
+    max-height: 220px;
+  }
+
+  .ie-banner__close__a {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    height: 20px;
+    width: 20px;
+  }
+
+  .ie-banner__p {
+    font-size: 14px;
+    line-height: 18px;
+    margin: 1rem 0 2rem 0.5rem;
+  }
+`;
