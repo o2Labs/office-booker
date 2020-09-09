0e2579e4faefa8d924ede0a7478e32ddbd804808
Daniel Bradley <daniel.bradley@telefonica.com>
2020-09-09 10:57:43 +0100

    Fix new register process
    - Fix crypto import to use nodejs module.
    - Catch UsernameExistsException.
    - Remove unnecessary changing of password.

--- server/users/register.ts
+++ server/users/register.ts
@@ -1,6 +1,7 @@
 import { Config } from '../app-config';
 import { CognitoIdentityServiceProvider } from 'aws-sdk';
 import { ensureUserExists } from '../db/users';
+import { randomBytes } from 'crypto';
 
 export type RegisterBody = {
   action: 'Register';
@@ -10,14 +11,9 @@ export type RegisterBody = {
 export const isRegisterBody = (input: any): input is RegisterBody =>
   typeof input === 'object' && input.action === 'Register' && typeof input.email === 'string';
 
-const intToHex = (nr: number) => nr.toString(16).padStart(2, '0');
-
 const getRandomString = (bytes: number) => {
-  const randomValues = new Uint8Array(bytes);
-
-  crypto.getRandomValues(randomValues);
-
-  return Array.from(randomValues).map(intToHex).join('');
+  const randomValues = randomBytes(bytes);
+  return randomValues.toString('base64');
 };
 
 export const registerUser = async (config: Config, email: string) => {
@@ -32,21 +28,18 @@ export const registerUser = async (config: Config, email: string) => {
   });
 
   const cognito = new CognitoIdentityServiceProvider();
-  await cognito
-    .adminCreateUser({
-      UserPoolId: config.authConfig.cognitoUserPoolId,
-      Username: email,
-      MessageAction: 'SUPPRESS',
-    })
-    .promise();
-  // Get rid of temporary password.
-  await cognito
-    .adminSetUserPassword({
-      UserPoolId: config.authConfig.cognitoUserPoolId,
-      Username: email,
-      Password: getRandomString(20),
-      Permanent: true,
-    })
-    .promise();
+  try {
+    await cognito
+      .adminCreateUser({
+        UserPoolId: config.authConfig.cognitoUserPoolId,
+        Username: email,
+        MessageAction: 'SUPPRESS',
+      })
+      .promise();
+  } catch (err) {
+    if (err.code !== 'UsernameExistsException') {
+      throw err;
+    }
+  }
   return;
 };
