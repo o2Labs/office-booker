bf00a16cd8bfd707239d3c9f5c513057a257b877
aangelisc <andreas.christou@telefonica.com>
2020-11-11 10:57:06 +0000

    Reconfigure MSW to have separate handlers for each test

--- client/src/components/App/Admin/Bookings.test.tsx
+++ client/src/components/App/Admin/Bookings.test.tsx
@@ -3,10 +3,7 @@ import { render, fireEvent, waitFor, screen, queries } from '@testing-library/re
 import '@testing-library/jest-dom/extend-expect';
 import Bookings from './Bookings';
 import { configureAuth } from '../../../lib/auth';
-import {
-  mockBookingsResponse,
-  mockOfficesResponse,
-} from '../../../../test/server.mock';
+import { server } from '../../../../test/server.mock';
 import {
   createFakeBooking,
   createFakeConfig,
@@ -14,14 +11,26 @@ import {
   createFakeSystemAdminUser,
 } from '../../../../test/data';
 import { TestContext } from '../../../../test/TestContext';
+import { rest } from 'msw';
+import { Office } from '../../../types/api';
+
+function mockGetOffices(officesResponse: Office[]) {
+  return rest.get('/api/offices', (req, res, ctx) => {
+    return res(ctx.json(officesResponse));
+  });
+}
 
 test('No bookings', async () => {
   const config = createFakeConfig();
   const office = createFakeOffice();
   const user = createFakeSystemAdminUser([office]);
+  server.use(
+    mockGetOffices([office]),
+    rest.get('/api/bookings', (req, res, ctx) => {
+      return res(ctx.json([]));
+    })
+  );
   configureAuth(config);
-  mockOfficesResponse([office]);
-  mockBookingsResponse([]);
   render(
     <TestContext user={user} config={config}>
       <Bookings />
@@ -38,9 +47,13 @@ test('Has one booking', async () => {
   const office = createFakeOffice();
   const user = createFakeSystemAdminUser([office]);
   const booking = createFakeBooking({ office, user: 'bookinguser@domain.test' });
+  server.use(
+    mockGetOffices([office]),
+    rest.get('/api/bookings', (req, res, ctx) => {
+      return res(ctx.json([booking]));
+    })
+  );
   configureAuth(config);
-  mockOfficesResponse([office]);
-  mockBookingsResponse([booking]);
   render(
     <TestContext user={user} config={config}>
       <Bookings />
@@ -52,4 +65,46 @@ test('Has one booking', async () => {
   });
   screen.getByText('Cancel');
   expect(screen.getByText(/Bookings:/)?.nextSibling).toHaveTextContent('1');
+});
+
+test('Can cancel booking', async () => {
+  const config = createFakeConfig();
+  const office = createFakeOffice();
+  const user = createFakeSystemAdminUser([office]);
+  const booking = createFakeBooking({ office, user: 'bookinguser@domain.test' });
+  configureAuth(config);
+  const deleteReq = jest.fn();
+  server.use(
+    mockGetOffices([office]),
+    rest.get('/api/bookings', (req, res, ctx) => {
+      return res(ctx.json([booking]));
+    }),
+    rest.delete('/api/bookings/booking', (req, res, ctx) => {
+      deleteReq(req.url.href);
+      return res(ctx.status(200));
+    })
+  );
+  render(
+    <TestContext user={user} config={config}>
+      <Bookings />
+    </TestContext>
+  );
+
+  await waitFor(() => {
+    screen.getByText('Cancel');
+  });
+
+  fireEvent.click(screen.getByText('Cancel'));
+
+  await waitFor(() => {
+    screen.getByText('Are you sure you want to cancel this booking?');
+  });
+  const dialog = screen.getByRole('dialog');
+  fireEvent.click(queries.getByText(dialog, 'Yes'));
+  await waitFor(() => {
+    screen.getByText('Booking cancelled');
+  });
+  expect(deleteReq).toHaveBeenCalledWith(
+    'http://localhost/api/bookings/booking?user=bookinguser@domain.test'
+  );
 });
--- client/test/server.mock.ts
+++ client/test/server.mock.ts
@@ -1,26 +1,7 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { Booking, Office } from '../src/types/api';
 
-let officesResponse: Office[] = [];
-
-export const mockOfficesResponse = (offices: Office[]): void => {
-  officesResponse = offices;
-};
-
-let bookingsResponse: Booking[] = [];
-
-export const mockBookingsResponse = (bookings: Booking[]): void => {
-  bookingsResponse = bookings;
-};
-
-const server = setupServer(
-  rest.get('/api/offices', (req, res, ctx) => {
-    return res(ctx.json(officesResponse));
-  }),
-  rest.get('/api/bookings', (req, res, ctx) => {
-    return res(ctx.json(bookingsResponse));
-  }),
+export const server = setupServer(
   rest.get('*', (req, res, ctx) => {
     console.log(req.method, req.url.href);
     return res(ctx.status(404));
@@ -42,6 +23,5 @@ const server = setupServer(
 beforeAll(() => server.listen());
 afterEach(() => {
   server.resetHandlers();
-  officesResponse = [];
 });
 afterAll(() => server.close());
