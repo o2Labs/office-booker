0214310365e7e2b8ae40b154bee36afd2cd81441
Daniel Bradley <daniel.bradley@telefonica.com>
2020-11-24 16:14:14 +0000

    Use bundle splitting for routes
    - Reduce initial load from 275kb -> 239kb.
    - Avoid larger components going into the admin pages affecting general loading times.

--- client/src/components/Routes.tsx
+++ client/src/components/Routes.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import Loadable from '@loadable/component';
 
+import Loading from './Assets/LoadingSpinner';
 import RequireLogin from './Auth/RequireLogin';
 import PageNotFound from './App/PageNotFound';
 import Home from './App/Home';
-import ViewBooking from './App/ViewBooking';
-import Help from './App/Help';
-import Bookings from './App/Admin/Bookings';
-import Users from './App/Admin/Users';
-import User from './App/Admin/User';
-import UserBookings from './App/Admin/UserBookings';
-import CreateBooking from './App/Admin/CreateBooking';
-import BookingStats from './App/Admin/BookingStats';
 import UpcomingBookings from './App/UpcomingBookings';
+import ViewBooking from './App/ViewBooking';
 import Privacy from './App/Privacy';
+import Help from './App/Help';
+import Layout from './Layout/Layout';
+
+const fallback = (
+  <Layout>
+    <Loading />
+  </Layout>
+);
+
+const Bookings = Loadable(() => import('./App/Admin/Bookings'), { fallback });
+const BookingStats = Loadable(() => import('./App/Admin/BookingStats'), { fallback });
+const CreateBooking = Loadable(() => import('./App/Admin/CreateBooking'), { fallback });
+const Users = Loadable(() => import('./App/Admin/Users'), { fallback });
+const User = Loadable(() => import('./App/Admin/User'), { fallback });
+const UserBookings = Loadable(() => import('./App/Admin/UserBookings'), { fallback });
 
 const Routes: React.FC = () => {
   // Render
